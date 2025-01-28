3fcd4c9c601da897dea56128795260ed02aa3315
shibatakeigo <sagankeigo17@gmail.com>
2025-01-28 19:57:59 +0900

    Fixed launch file

--- launch/rviz.launch.py
+++ launch/rviz.launch.py
@@ -39,7 +39,7 @@ def generate_launch_description():
         output='screen'
     )
     joint_velocity_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_start_controller', 'velocity_controllers'],
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active', 'velocity_controllers'],
         output='screen'
     )
 
@@ -60,64 +60,3 @@ def generate_launch_description():
         node_robot_state_publisher,
         spawn_entity,
     ])
-
-    # package_dir = get_package_share_directory("inrof2025_ros")
-    # urdf = os.path.join(package_dir, "urdf", "trolley.urdf")
-
-    # with open(urdf, 'r') as file:
-    #     robot_description_content = file.read()
-
-    # robot_state_publisher_gui = Node(
-    #     package="robot_state_publisher",
-    #     executable="robot_state_publisher",
-    #     output="both",
-    #     parameters=[{'robot_description': robot_description_content}],
-    # )
-
-    # joint_state_publisher_node = Node(
-    #     package="joint_state_publisher_gui",
-    #     executable="joint_state_publisher_gui",
-    #     name='joint_state_publisher_gui',
-    #     output="screen"
-    # )
-
-    # rviz_node = Node(
-    #     package="rviz2",
-    #     executable="rviz2",
-    #     name="rviz2",
-    #     output="log",
-    # )
-
-    # gazebo = IncludeLaunchDescription(
-    #     PythonLaunchDescriptionSource([os.path.join(
-    #         get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
-    # )
-
-    # spawn_entity = Node(
-    #     package='gazebo_ros', executable='spawn_entity.py',
-    #     arguments=[
-    #         '-topic', 'robot_description',
-    #         '-entity', 'diffbot'
-    #     ],
-    #     output='screen')
-
-    # load_joint_state_controller = ExecuteProcess(
-    #     cmd=['ros2', 'control', 'load_controller', '--set-state', 'active', 'joint_state_broadcaster'],
-    #     output='screen'
-    # )
-    # load_joint_group_velocity_controller = ExecuteProcess(
-    #     cmd=['ros2', 'control', 'load_controller', '--set-state', 'active', 'joint_group_velocity_controller'],
-    #     output='screen'
-    # )
-
-    # nodes_to_run = [
-    #     robot_state_publisher_gui,
-    #     joint_state_publisher_node,
-    #     rviz_node,
-    #     # gazebo,
-    #     load_joint_state_controller,
-    #     # spawn_entity,
-    #     # load_joint_group_velocity_controller
-    # ]
-
-    # return LaunchDescription(nodes_to_run)
