d4dd5968f6b038ccbefa636a58252b24c90238a0
shibatakeigo <sagankeigo17@gmail.com>
2025-06-13 23:11:20 +0900

    Added delay

--- include/ros_node.hpp
+++ include/ros_node.hpp
@@ -14,7 +14,7 @@ class BTNode: public rclcpp::Node {
     public:
         explicit BTNode(const rclcpp::NodeOptions & options = rclcpp::NodeOptions()): Node("bt_node", options) {
             // create service client
-            srvGenRoute_ = this->create_client<inrof2025_ros_type::srv::GenRoute>("/srv/vacume");
+            srvGenRoute_ = this->create_client<inrof2025_ros_type::srv::GenRoute>("generate_route");
             while (!srvGenRoute_->wait_for_service(1s))
             {
                 if (!rclcpp::ok()) {
